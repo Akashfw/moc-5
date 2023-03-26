840891bc936e1dd3fac2e717ff6547043dcff70e
unknown <shuklaakash30278@gmail.com>
2023-03-25 21:20:16 -0700

    redis -2

--- unit-5/sprint-4/day-2/assignments/redis_assignment-2/index.js
+++ unit-5/sprint-4/day-2/assignments/redis_assignment-2/index.js
@@ -1,21 +1,28 @@
 const express= require("express");
 const app= express();
 app.use(express.text());
+app.use(express.json());
 const redis= require("redis");
 require("dotenv").config();
 
 
+// const client = redis.createClient({
+//     password: process.env.pass,
+//     socket: {
+//       host: "redis-14279.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+//       port: 14279,
+//     },
+//   });
+
 const client = redis.createClient({
-    password: process.env.pass,
-    socket: {
-      host: "redis-14279.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-      port: 14279,
-    },
-  });
+  url:`redis://default:${process.env.pass}@redis-19330.c212.ap-south-1-1.ec2.cloud.redislabs.com:19330`
+})
 
   client.on("error", (err) => console.log(err, "ERROR in REDIS"));
-client.connect();
 
+   client.on("ready",()=>{
+       console.log("connected to redis")
+   })
 
 app.post("/message/:key", async (req, res) => {
     const { key } = req.params;
@@ -35,8 +42,16 @@ app.post("/message/:key", async (req, res) => {
     const ttl= await client.TTL(key);
     res.send("Success! " + `${key} : ${payload} will expire in ${ttl} seconds`);
   });
+
+  app.get("/message/:key",async(req,res)=>{
+    const {key}= req.params;
+    const ans= await client.GET(key);
+
+    res.send(ans);
+  })
   
-  app.listen(process.env.port, () => {
+  app.listen(process.env.port, async () => {
+      await client.connect();
       console.log("Listening at  "+`${process.env.port}`);
     });
 
