b391f01c8e7ffe622fedf55d8438b0477269794a
Akashfw <shuklaakash30278@gmail.com>
2023-04-17 16:18:40 +0530

    assignment

---
+++ unit-6/sprint-2/evaluation/player/src/code.ts
@@ -0,0 +1,98 @@
+
+
+
+export class Entity{
+    health : number;
+   strength : number;
+   defense : number;
+   name : string;
+   
+   constructor(health : number,strength : number,defense : number,name : string) {
+       this.health=health,
+       this.strength=strength,
+       this.defense=defense,
+       this.name=name
+   }
+
+   movement(){
+
+   }
+
+}
+
+
+// Different Player Characters
+export class Player extends Entity{
+    level : Number;
+
+    constructor(health: number, strength: number, defense: number, name: string){
+        super(health,strength,defense,name)
+        this.level=1
+    }
+    attack(){
+
+    }
+}
+
+export class Swordsman extends Player {
+    name:string
+    constructor(health: number, strength: number, defense: number, name: string) {
+        
+         super(health,strength,defense,name);Player;
+         this.name = "Swordsman"
+    }
+    
+    slashAttack(){
+
+    }
+}
+export class Mage extends Player {
+    constructor(health: number, strength: number, defense: number, name: string){
+        super(health,strength,defense,"Mage");
+        this.name= "Mage"
+    }
+    magicAttack(){
+
+    }
+}
+
+export class Spearman extends Player {
+    constructor(health: number, strength: number, defense: number, name: string){
+        super(health,strength,defense,"Spearman");
+        this.name= "Spearman"
+    }
+    stabAttack(){
+        
+    }
+}
+
+// All Enemies
+export class Enemy  extends Entity {
+    followPlayer(){
+
+    }
+}
+
+export class Zombies extends Enemy  {
+    constructor(health: number, strength: number, defense: number, name: string){
+        super(health,strength,defense,"Zombie");
+        this.name= "Zombie"
+    }
+    poisonAttack(){
+        
+    }
+
+}
+
+export class Werewolf extends Enemy {
+    constructor(health: number, strength: number, defense: number, name: string){
+        super(health,strength,defense,"Werewolf");
+        this.name= "Werewolf"
+    }
+    biteAttack(){
+        
+    }
+    roar(){
+        
+    }
+}
---
+++ unit-6/sprint-2/evaluation/player/src/index.ts
@@ -0,0 +1,7 @@
+import { Werewolf } from "./code";
+  const p = new Werewolf(100,20,30);
+  console.log(p.movement);
+  console.log(p.followPlayer);
+  console.log(p.biteAttack);
+  console.log(p.roar);
+  console.log(p);
