e8c1c96c80b6892e3e88626ac3024bd6c4e6c09d
Akashfw <shuklaakash30278@gmail.com>
2023-04-17 16:52:40 +0530

    assignment-1

---
+++ unit-6/sprint-2/evaluation/hotel/src/index.ts
@@ -0,0 +1,102 @@
+class hotels {
+    type: "3star" | "5star" | "Resort";
+    name:string;
+    location:string;
+    
+    constructor(type:"3star" | "5star" | "Resort", name:string, location:string){
+        this.type=type,
+        this.name=name,
+        this.location=location
+    }
+     checkRoomAvailability(check:room[]){
+         let count=0;
+         for(let i=0; i<check.length; i++){
+             if(check[i].isAvailable==true){
+                 count++;
+             }
+         }
+         return count;
+     }
+}
+
+interface room {
+    roomNumber:number,
+    isAvailable:boolean
+}
+
+class Standard extends hotels {
+    NumberOfBeds:number;
+    AC:boolean;
+    price:number;
+
+    constructor(NumberOfBeds:number,AC:boolean,price:number,type:"3star" | "5star" | "Resort", name:string, location:string){
+        super(type, name, location)
+        this.NumberOfBeds=NumberOfBeds;
+        if(type == "5star"){
+            this.AC= true
+        }else{
+            this.AC=AC
+        }
+        this.price=price
+    }
+
+    bookRoom(Room:room){
+        if(Room.isAvailable == true){
+            Room.isAvailable=false;
+            return true
+        }else{
+            return false
+        }
+
+    }
+}
+
+
+class Delux extends hotels {
+    NumberOfBeds:number;
+    AC:boolean;
+    price:number;
+
+    constructor(NumberOfBeds:number,AC:boolean,price:number,type:"3star" | "5star" | "Resort", name:string, location:string){
+        super(type, name, location)
+        this.NumberOfBeds=NumberOfBeds;
+        if(type == "5star"){
+            this.AC= true
+        }else{
+            this.AC=AC
+        }
+        this.price=price
+    }
+    bookRoom(Room:room){
+        if(Room.isAvailable == true){
+            Room.isAvailable=false;
+            return true
+        }else{
+            return false
+        }
+
+    }
+}
+
+
+class Premium extends hotels {
+    NumberOfBeds:number;
+    AC:boolean;
+    price:number;
+
+    constructor(NumberOfBeds:number,AC:boolean,price:number,type:"3star" | "5star" | "Resort", name:string, location:string){
+        super(type, name, location)
+        this.NumberOfBeds=NumberOfBeds;
+        this.AC= true;
+        this.price=price;
+    }
+    bookRoom(Room:room){
+        if(Room.isAvailable == true){
+            Room.isAvailable=false;
+            return true
+        }else{
+            return false
+        }
+
+    }
+}
