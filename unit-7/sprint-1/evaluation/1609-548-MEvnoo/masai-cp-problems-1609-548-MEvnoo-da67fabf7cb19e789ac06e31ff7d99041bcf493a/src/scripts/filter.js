876531eb2bba3d9c1b19290e45793f5bb5714f08
Akashfw <shuklaakash30278@gmail.com>
2023-05-20 13:53:27 +0530

    mock2-6

--- unit-7/sprint-1/evaluation/1609-548-MEvnoo/masai-cp-problems-1609-548-MEvnoo-da67fabf7cb19e789ac06e31ff7d99041bcf493a/src/scripts/filter.js
+++ unit-7/sprint-1/evaluation/1609-548-MEvnoo/masai-cp-problems-1609-548-MEvnoo-da67fabf7cb19e789ac06e31ff7d99041bcf493a/src/scripts/filter.js
@@ -32,9 +32,6 @@ function fill_year(){
     if(!year){
         year=2011
     };
-    if(year==2011 && team1=="" && team2==""){
-        fetch(`https://jsonmock.hackerrank.com/api/football_matches?page=2&year=${year}`).then((res)=>res.json()).then((val)=> display(val.data)).catch((err)=>alert(err));
-    }
     if(team1=="" && team2==""){
         fetch(`https://jsonmock.hackerrank.com/api/football_matches?year=${year}`).then((res)=>res.json()).then((val)=> display(val.data)).catch((err)=>alert(err));
     }else if(team1=="" && team2!=""){
